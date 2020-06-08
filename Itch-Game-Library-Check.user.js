a9e34905e3438fdc1ea5f94454b5de6e3621164d
HCLonely <h1606051253@gmail.com>
2020-06-08 16:58:21 +0800

    添加白名单、黑名单模式，绿色加深

--- Itch-Game-Library-Check.user.js
+++ Itch-Game-Library-Check.user.js
@@ -1,24 +1,27 @@
+/* eslint-disable no-void,no-func-assign,no-fallthrough,no-unsafe-finally,no-mixed-operators */
 function _toConsumableArray (arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread() }
 
 function _nonIterableSpread () { throw new TypeError('Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.') }
-
-function _unsupportedIterableToArray (o, minLen) { if (!o) return; if (typeof o === 'string') return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === 'Object' && o.constructor) n = o.constructor.name; if (n === 'Map' || n === 'Set') return Array.from(o); if (n === 'Arguments' || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen) }
 
 function _iterableToArray (iter) { if (typeof Symbol !== 'undefined' && Symbol.iterator in Object(iter)) return Array.from(iter) }
 
 function _arrayWithoutHoles (arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr) }
 
-function _arrayLikeToArray (arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i] } return arr2 }
-
 function asyncGeneratorStep (gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value } catch (error) { reject(error); return } if (info.done) { resolve(value) } else { Promise.resolve(value).then(_next, _throw) } }
 
 function _asyncToGenerator (fn) { return function () { var self = this; var args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next (value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'next', value) } function _throw (err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, 'throw', err) } _next(undefined) }) } }
+
+function _createForOfIteratorHelper (o, allowArrayLike) { var it; if (typeof Symbol === 'undefined' || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === 'number') { if (it) o = it; var i = 0; var F = function F () {}; return { s: F, n: function n () { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] } }, e: function e (_e2) { throw _e2 }, f: F } } throw new TypeError('Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.') } var normalCompletion = true; var didErr = false; var err; return { s: function s () { it = o[Symbol.iterator]() }, n: function n () { var step = it.next(); normalCompletion = step.done; return step }, e: function e (_e3) { didErr = true; err = _e3 }, f: function f () { try { if (!normalCompletion && it.return != null) it.return() } finally { if (didErr) throw err } } } }
+
+function _unsupportedIterableToArray (o, minLen) { if (!o) return; if (typeof o === 'string') return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === 'Object' && o.constructor) n = o.constructor.name; if (n === 'Map' || n === 'Set') return Array.from(o); if (n === 'Arguments' || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen) }
+
+function _arrayLikeToArray (arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i] } return arr2 }
 
 // ==UserScript==
 // @name           游戏库检测-itch
 // @name:en        Itch Game Library Check
 // @namespace      itch-game-library-check
-// @version        1.0.0
+// @version        1.0.1
 // @description    检测itch.io游戏是否已拥有
 // @description:en Check if the game of itch.io is already owned
 // @author         HCLonely
@@ -44,9 +47,41 @@ function _asyncToGenerator (fn) { return function () { var self = this; var args
 // ==/UserScript==
 
 /* global $,Swal,GM_getValue,GM_setValue,GM_openInTab,GM_xmlhttpRequest,GM_addStyle,GM_registerMenuCommand,regeneratorRuntime */
-
-/* eslint-disable no-void,no-func-assign,no-fallthrough */
 (function () {
+  var whiteList = GM_getValue('whiteList') || []
+  var blackList = GM_getValue('blackList') || []
+  var url = window.location.href
+
+  if (whiteList.length > 0) {
+    var _iterator = _createForOfIteratorHelper(whiteList)
+    var _step
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var e = _step.value
+        if (!url.includes(e)) return
+      }
+    } catch (err) {
+      _iterator.e(err)
+    } finally {
+      _iterator.f()
+    }
+  } else if (blackList.length > 0) {
+    var _iterator2 = _createForOfIteratorHelper(blackList)
+    var _step2
+
+    try {
+      for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+        var _e = _step2.value
+        if (url.includes(_e)) return
+      }
+    } catch (err) {
+      _iterator2.e(err)
+    } finally {
+      _iterator2.f()
+    }
+  }
+
   if (getItchGameLibrary().length === 0) {
     Swal.fire({
       title: '游戏库检测脚本提醒',
@@ -232,7 +267,8 @@ function _asyncToGenerator (fn) { return function () { var self = this; var args
                 return _context.abrupt('return', Swal.update({
                   icon: 'error',
                   title: 'itch游戏库数据更新失败',
-                  text: '详情请查看控制台'
+                  text: '详情请查看控制台',
+                  showCancelButton: true
                 }))
 
               case 25:
@@ -256,6 +292,34 @@ function _asyncToGenerator (fn) { return function () { var self = this; var args
     })
   }
 
+  function addWhiteList () {
+    Swal.fire({
+      title: '添加白名单',
+      input: 'textarea',
+      inputValues: whiteList.join('\n'),
+      confirmButtonText: '保存',
+      cancelButtonText: '取消'
+    }).then(function (_ref4) {
+      var value = _ref4.value
+      if (value) GM_setValue('whiteList', value.split('\n'))
+    })
+  }
+
+  function addBlackList () {
+    Swal.fire({
+      title: '添加黑名单',
+      input: 'textarea',
+      inputValues: blackList.join('\n'),
+      confirmButtonText: '保存',
+      cancelButtonText: '取消'
+    }).then(function (_ref5) {
+      var value = _ref5.value
+      if (value) GM_setValue('blackList', value.split('\n'))
+    })
+  }
+
   GM_registerMenuCommand('更新itch游戏库', updateItchGameLibrary)
-  GM_addStyle('.itch-io-game-link-owned{background:#16c60c !important}')
+  GM_registerMenuCommand('白名单', addWhiteList)
+  GM_registerMenuCommand('黑名单', addBlackList)
+  GM_addStyle('.itch-io-game-link-owned{background:#008000 !important}')
 })()
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ babel.transform(itchJs, {}, function (err, result) {
   if (err) {
     return console.error('babel转换失败: ', err)
   }
-  fs.writeFile('./Itch-Game-Library-Check.user.js', result.code, function (error) {
+  fs.writeFile('./Itch-Game-Library-Check.user.js', '/* eslint-disable no-void,no-func-assign,no-fallthrough,no-unsafe-finally,no-mixed-operators */\n' + result.code, function (error) {
     if (error) {
       return console.error('auto-task.user.js文件写入失败: ', error)
     }
